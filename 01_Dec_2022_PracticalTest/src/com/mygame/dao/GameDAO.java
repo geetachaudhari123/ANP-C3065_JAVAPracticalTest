05b00b63d1a1fe6841531422ff403f75682bfa23
Mrunali <Mrunali@DESKTOP-TV8QIGR>
2022-12-02 19:54:28 +0530

    version1

--- 01_Dec_2022_PracticalTest/src/com/mygame/dao/GameDAO.java
+++ 01_Dec_2022_PracticalTest/src/com/mygame/dao/GameDAO.java
@@ -1,5 +1,26 @@
 package com.mygame.dao;
 
 public class GameDAO {
+		// class
+		String cityName[]={"chennai","kolkata","delhi","mumbai"};
+		//variable 
+public void getcityName() {
+		//method 
+		//String cityName=Name;  
+		for(int i=0;i<cityName.length;i++) 
+			{
+			System.out.println(cityName[i]);
+			}
+	  }
+public static void main(String[]args){
+		{
+		  
+			GameDao GameDaobj=new GameDao();
+			GameDaobj.getcityName();
+		         
+		}
+	}
+            
 
-}
+	} 
+
